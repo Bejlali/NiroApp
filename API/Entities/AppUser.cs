5750e025640e16e370fd6c673216649bcb4601a6
kingfish60 <babak.ejlali@gmail.com>
2023-06-01 15:37:27 -0400

    sec 8-1

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,21 +1,40 @@
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 namespace API.Entities
 {
     public class AppUser
     {
         [Key]
-       public int Id { get; set; } 
-       public string UserName { get; set; }
-       public string FName { get; set; }
-       public string LName { get; set; } 
-       public string Email {get; set; }
-       public string Profile {get; set; }
-       public string Department {get; set; }
-       public string Phone {get; set; }
-       public string Address {get; set; }
-       public byte[] PasswordHash{ get; set; }
-       public byte[] PasswordSalt{ get; set; }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string Profile { get; set; }
+        public string Department { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new List<Photo>();
+        public int GetAge()
+        {
+            return DateOfBirth.CalcuateAge();
+        }
+
 
     }
+
+
 }
