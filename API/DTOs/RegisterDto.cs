78ccfc808c6c96aeefa551ed423d5ac1ed4974ec
kingfish60 <babak.ejlali@gmail.com>
2023-05-31 23:16:26 -0400

    SEC 7-1

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(32, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,5 @@
-using API.Services;
 using API.Extensions;
+using API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
-//builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
+
 app.UseAuthentication();
 app.UseAuthorization();
 
