8a33ac8de7e3e2e1b35025c80b974993255a6095
kingfish60 <babak.ejlali@gmail.com>
2023-06-02 14:28:25 -0400

    sec 8 -8 - added helper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -9,10 +9,10 @@ namespace API.Helpers
     {
          public AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
-/*                 .ForMember(dest => dest.PhotoUrl, 
-                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
-                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalcuateAge())); */
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, 
+                opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalcuateAge()));
 
             CreateMap<Photo, PhotoDto>();
         }
