406b1c6503e7c713d2648f8586b52353c072cd1c
kingfish60 <babak.ejlali@gmail.com>
2023-06-02 12:27:22 -0400

    sec 8-3

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,39 @@
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+            
+        }
+        public Task<AppUser> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
