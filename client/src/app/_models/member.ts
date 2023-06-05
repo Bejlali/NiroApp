55d39aee23d59b97aa5a7e5c6c770ebc930303b9
kingfish60 <babak.ejlali@gmail.com>
2023-06-05 09:55:30 -0400

    sec 9-1

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,27 @@
+import { Photo } from "./photo";
+
+export interface Member {
+  id:           number;
+  userName:     string;
+  photoUrl:     string;
+  age:          number;
+  knownAs:      string;
+  created:      Date;
+  lastActive:   Date;
+  gender:       string;
+  introduction: string;
+  lookingFor:   string;
+  interests:    string;
+  city:         string;
+  country:      string;
+  photos:       Photo[];
+  fName:        string;
+  lName:        string;
+  email:        string;
+  profile:      string;
+  department:   string;
+  phone:        string;
+  address:      string;
+}
+
+
