8d42b3045b3b2f0d3d3f1c279b2f5dfc9186941a
kingfish60 <babak.ejlali@gmail.com>
2023-06-08 15:22:55 -0400

    Sec11-1(photo  service added)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -12,50 +13,55 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [Authorize]
-   /*[ApiController]
-    [Route("api/[controller]")]  // /api/user ===> UserController ==>api/user*/
+    /*[ApiController]
+     [Route("api/[controller]")]  // /api/user ===> UserController ==>api/user*/
     public class UsersController : BaseApiController
     {
-       // private readonly DataContext _context;
+        // private readonly DataContext _context;
         //public UsersController(DataContext context)
-            //_context = context;
+        //_context = context;
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UsersController( IUserRepository userRepository, IMapper mapper)
+        private readonly IPhotoService _photoService;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
+            _photoService = photoService;
             _mapper = mapper;
             _userRepository = userRepository;
-            
+
         }
 
-       //[AllowAnonymous]
+        //[AllowAnonymous]
         [HttpGet]
         //public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-           var users = await _userRepository.GetMembersAsync();
-           //var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-           // return Ok(usersToReturn);
-           return Ok(users);
+            var users = await _userRepository.GetMembersAsync();
+            //var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            // return Ok(usersToReturn);
+            return Ok(users);
         }
 
         [HttpGet("{username}")]
         //public async Task<ActionResult<AppUser>> GetUser(string username)
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-/*          var user = await _context.Users.FindAsync(id); // or return _context.Users.Find(id); 
-            return user; */
+            /*          var user = await _context.Users.FindAsync(id); // or return _context.Users.Find(id); 
+                        return user; */
             //return await _context.Users.FindAsync(id);
             //return await _userRepository.GetUserByUsernameAsync(username);
             // var user =  await _userRepository.GetUserByUsernameAsync(username);
             // return _mapper.Map<MemberDto>(user);
-           return await _userRepository.GetMemberAsync(username); 
+            return await _userRepository.GetMemberAsync(username);
 
         }
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            //var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var username = User.GetUsername();
+            
             var user = await _userRepository.GetUserByUsernameAsync(username);
 
             if (user == null) return NotFound();
@@ -65,6 +71,81 @@ namespace API.Controllers
             if (await _userRepository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to update user");
+        }
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            // var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            if (user == null) return NotFound();
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0) photo.IsMain = true;
+
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsync())
+            {
+                return CreatedAtAction(nameof(GetUser),
+                    new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+            }
+
+            return BadRequest("Problem adding photo");
+        }
+
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            if (user == null) return NotFound();
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("this is already your main photo");
+
+            var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+            if (currentMain != null) currentMain.IsMain = false;
+            photo.IsMain = true;
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Problem setting the main photo");
+        }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("You cannot delete your main photo");
+
+            if (photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if (await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting photo");
         }
 
 
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,33 +2,37 @@ using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using API.Helpers;
 
 namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
     {
-          public static IServiceCollection AddApplicationServices(this IServiceCollection services,
-            IConfiguration config)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+          IConfiguration config)
 
         {
-        services.AddDbContext<DataContext>(opt => 
-        {
-            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
-        });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+            });
 
-        /* builder.Services.AddCors();
-        builder.Services.AddScoped<ITokenService, TokenService>();
-        builder.Services.AddApplicationServices(builder.Configuration);
-        builder.Services.AddIdentityServices(builder.Configuration);
-        */
-        services.AddCors();
-        services.AddScoped<ITokenService, TokenService>();
-        services.AddScoped<IUserRepository, UserRepository>();
-         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-        return services;
+            /* builder.Services.AddCors();
+            builder.Services.AddScoped<ITokenService, TokenService>();
+            builder.Services.AddApplicationServices(builder.Configuration);
+            builder.Services.AddIdentityServices(builder.Configuration);
+            */
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+             services.AddScoped<IPhotoService, PhotoService>();
+
+            return services;
 
         }
-     
-        
+
+
     }
 }
