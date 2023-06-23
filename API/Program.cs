17e13bb67c8dd40a8253694915278e57ac61dee2
kingfish60 <babak.ejlali@gmail.com>
2023-06-23 10:54:49 -0400

    sec 15-3(new database migiration)

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 using System.Text.Json.Serialization;
 using API.Data;
+using API.Entities;
 using API.Extensions;
 using API.Middleware;
 using API.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,8 +40,9 @@ var services = scope.ServiceProvider;
 try 
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedUsers(context);
+    await Seed.SeedUsers(userManager);
 }
 catch (Exception ex)
 {
